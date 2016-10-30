be03e999d774229366b0851f198f2aaff1750860
yong.fu <hope_fu@163.com>
2016-10-30 17:51:16 +0800

    support https request

--- src/main/java/org/allen/remote/rest/HttpClientSample.java
+++ src/main/java/org/allen/remote/rest/HttpClientSample.java
@@ -3,20 +3,27 @@ package org.allen.remote.rest;
 import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.sun.istack.internal.NotNull;
-import org.apache.http.HttpResponse;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -26,14 +33,47 @@ import java.util.Map;
  */
 public class HttpClientSample {
 
-    private HttpClient httpClient;
+    private CloseableHttpClient httpClient;
 
     public HttpClientSample() {
         // simple http client
-        httpClient = HttpClientBuilder.create().build();
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            X509TrustManager tm = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {
+                }
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {
+                }
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+
+            sslContext.init(null, new TrustManager[]{tm}, null);
+            httpClient = HttpClientBuilder.create()
+                    .setSSLContext(sslContext)
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public <T> T doHttpGet(@NotNull String url, Map<String, String> parameters, Class<T> t) throws IOException {
+    /**
+     * get request
+     * @param url
+     * @param parameters
+     * @param t
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
+    public <T> T get(@NotNull String url, Map<String, String> parameters, Class<T> t) throws Exception {
         if (parameters != null && parameters.size() > 0) {
             StringBuilder queryString = new StringBuilder("?");
             for (Map.Entry<String, String> entry : parameters.entrySet()) {
@@ -43,24 +83,20 @@ public class HttpClientSample {
         }
 
         HttpGet httpGet = new HttpGet(url);
-        long t1 = System.currentTimeMillis();
-        HttpResponse httpResponse = httpClient.execute(httpGet);
-        long t2 = System.currentTimeMillis();
-        System.out.println(String.format("############### request: %s, took: %s", url, (t2 - t1)));
-        String content = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-        if (t == String.class) {
-            return (T) content;
-        } else {
-            long t3 = System.currentTimeMillis();
-            T ret = JSON.parseObject(content, t);
-//            T ret = new Gson().fromJson(content, t);
-            long t4 = System.currentTimeMillis();
-            System.out.println("#################### parseJson took: " + (t4 - t3));
-            return ret;
-        }
+        return doExecute(httpGet, t);
     }
 
-    public <T> T doHttpPost(@NotNull String url, Map<String, String> parameters, Class<T> t) throws Exception {
+    /**
+     * post request
+     * support urlencoded
+     * @param url
+     * @param parameters
+     * @param t
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
+    public <T> T post(@NotNull String url, Map<String, String> parameters, Class<T> t) throws Exception {
         HttpPost httpPost = new HttpPost(url);
         if (parameters != null && parameters.size() > 0) {
             List<NameValuePair> params = new ArrayList<NameValuePair>();
@@ -70,42 +106,63 @@ public class HttpClientSample {
             httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
         }
 
-        long t1 = System.currentTimeMillis();
-        HttpResponse httpResponse = httpClient.execute(httpPost);
-        long t2 = System.currentTimeMillis();
-        System.out.println(String.format("##### request: %s, took: %s", url, (t2-t1)));
-        String content = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-        if (t == String.class) {
-            return (T) content;
-        } else {
-            long t3 = System.currentTimeMillis();
-            T ret = JSON.parseObject(content, t);
-//            T ret = new Gson().fromJson(content, t);
-            long t4 = System.currentTimeMillis();
-            System.out.println("#################### parseJson took: " + (t4 - t3));
-            return ret;
-        }
+        return doExecute(httpPost, t);
     }
 
-    public <T> T doHttpPostJson(String url, String json, Class<T> t) throws Exception {
+    /**
+     * post request
+     * support json body
+     * @param url
+     * @param json
+     * @param t
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
+    public <T> T postJson(String url, String json, Class<T> t) throws Exception {
         StringEntity stringEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
         HttpPost httpPost = new HttpPost(url);
         httpPost.setEntity(stringEntity);
+        return doExecute(httpPost, t);
+    }
 
-        long t1 = System.currentTimeMillis();
-        HttpResponse httpResponse = httpClient.execute(httpPost);
-        long t2 = System.currentTimeMillis();
-        System.out.println(String.format("##### request: %s, took: %s", url, (t2-t1)));
-        String content = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-        if (t == String.class) {
-            return (T) content;
-        } else {
-            long t3 = System.currentTimeMillis();
-            T ret = JSON.parseObject(content, t);
-//            T ret = new Gson().fromJson(content, t);
-            long t4 = System.currentTimeMillis();
-            System.out.println("#################### parseJson took: " + (t4 - t3));
-            return ret;
+    private <T> T doExecute(HttpUriRequest request, Class<T> t) throws IOException {
+        CloseableHttpResponse response = null;
+        InputStream in = null;
+        try {
+            long t1 = System.currentTimeMillis();
+            response = httpClient.execute(request);
+            long t2 = System.currentTimeMillis();
+            System.out.println(String.format("##### request: %s, took: %s", request.getURI(), (t2-t1)));
+            in = response.getEntity().getContent();
+            String content = IOUtils.toString(in, "UTF-8");
+            if (t == String.class) {
+                return (T) content;
+            } else {
+                long t3 = System.currentTimeMillis();
+                T ret = JSON.parseObject(content, t);
+                // T ret = new Gson().fromJson(content, t);
+                long t4 = System.currentTimeMillis();
+                System.out.println("#################### parseJson took: " + (t4 - t3));
+                return ret;
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    throw e;
+                }
+            }
+            if (response != null){
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    throw e;
+                }
+            }
         }
     }
 }
--- src/test/java/org/allen/remote/rest/HttpClientSampleTest.java
+++ src/test/java/org/allen/remote/rest/HttpClientSampleTest.java
@@ -5,7 +5,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,20 +27,20 @@ public class HttpClientSampleTest {
     }
 
     @Test
-    public void testDoHttpGet() throws IOException {
+    public void testDoHttpGet() throws Exception {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("format", "json");
         parameters.put("ip", "180.168.36.45");
-        String result = httpClientSample.doHttpGet("http://int.dpool.sina.com.cn/iplookup/iplookup.php", parameters, String.class);
+        String result = httpClientSample.get("http://int.dpool.sina.com.cn/iplookup/iplookup.php", parameters, String.class);
         System.out.println(result);
     }
 
     @Test
-    public void testDoHttpGet2() throws IOException {
+    public void testDoHttpGet2() throws Exception {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("format", "json");
         parameters.put("ip", "180.168.36.45");
-        IpLookupResponse result = httpClientSample.doHttpGet("http://int.dpool.sina.com.cn/iplookup/iplookup.php", parameters, IpLookupResponse.class);
+        IpLookupResponse result = httpClientSample.get("http://int.dpool.sina.com.cn/iplookup/iplookup.php", parameters, IpLookupResponse.class);
         System.out.println(new Gson().toJson(result));
         System.out.println("country: " + result.getCountry());
         System.out.println("province: " + result.getProvince());
@@ -49,11 +48,11 @@ public class HttpClientSampleTest {
     }
 
     @Test
-    public void testDoHttpGet3() throws IOException {
+    public void testDoHttpGet3() throws Exception {
         String url = "http://localhost:8080/springmvc/user/get-by-name";
         Map<String, String> param = new HashMap<>();
         param.put("name", "张%三");
-        String result = httpClientSample.doHttpGet(url, param, String.class);
+        String result = httpClientSample.get(url, param, String.class);
         System.out.println(result);
     }
 
@@ -62,14 +61,20 @@ public class HttpClientSampleTest {
         Map<String, String> param = new HashMap<>();
         param.put("id", "15");
         param.put("name", "张%三");
-        String result = httpClientSample.doHttpPost("http://localhost:8080/springmvc/user/add", param, String.class);
+        String result = httpClientSample.post("http://localhost:8080/springmvc/user/add", param, String.class);
         System.out.println(result);
     }
 
     @Test
     public void testDoHttpPostJson() throws Exception {
         String json = "{\"name\":\"孙大%圣\"}";
-        String result = httpClientSample.doHttpPostJson("http://localhost:8080/springmvc/user/create", json, String.class);
+        String result = httpClientSample.postJson("http://localhost:8080/springmvc/user/create", json, String.class);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDoHttpsGet() throws Exception {
+        String result = httpClientSample.get("https://localhost:8443/imocker/admin/query-all-api", null, String.class);
         System.out.println(result);
     }
 }
